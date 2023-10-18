07c1c925ab7f423b0b0b8267428d9ed53c8e89a8
Apidiya <128754190+apidiya@users.noreply.github.com>
2023-10-18 15:54:09 +0200

    soutenance OK

--- assets/script.js
+++ assets/script.js
@@ -1,83 +1,80 @@
-// Définition des diapositives
 const slides = [
-    {
-      "image": "slide1.jpg",
-      "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-    },
-    {
-      "image": "slide2.jpg",
-      "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-    },
-    {
-      "image": "slide3.jpg",
-      "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-    },
-    {
-      "image": "slide4.png",
-      "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-    }
-  ];
-  
-  // Sélection des éléments du DOM
-  const arrowLeft = document.querySelector('.arrow_left');
-  const arrowRight = document.querySelector('.arrow_right');
-  const dotsContainer = document.querySelector('.dots');
-  const slideImage = document.querySelector('.banner-img');
-  const slideText = document.querySelector('#banner p');
-  const dots = [];
-  
-    // Ajout d'un event listener pour la flèche droite
-    arrowRight.addEventListener('click', function () {
-      currentIndex = (currentIndex + 1) % numberOfSlides;
-      updateSlide(currentIndex);
-    });
-    
-    // Ajout d'un event listener pour la flèche gauche
-    arrowLeft.addEventListener('click', function () {
-      currentIndex = (currentIndex - 1 + numberOfSlides) % numberOfSlides;
-      updateSlide(currentIndex);
-    });
-    
-  // Index de la diapositive actuelle
-  let currentIndex = 0;
-  
-  // Fonction pour mettre à jour la diapositive
-  function updateSlide(index) {
-    const path = './assets/images/slideshow/';
-  
-    // Mettre à jour l'image
-    slideImage.src = `${path}${slides[index].image}`;
-  
-    // Mettre à jour le texte
-    slideText.innerHTML = slides[index].tagLine;
-  
-    // Mettre à jour la classe du bullet point actif
-    dots.forEach((dot, i) => {
-      if (i === index) {
-        dot.classList.add('dot_selected');
-      } else {
-        dot.classList.remove('dot_selected');
-      }
-    });
-  }
-  
-  // Nombre de diapositives
-  const numberOfSlides = slides.length;
-  
-  // Création et ajout des bullet points au HTML
-  for (let i = 0; i < numberOfSlides; i++) {
-    const dot = document.createElement('div');
-    dot.classList.add('dot');
-    dots.push(dot); // Ajouter le point au tableau des points
-    dotsContainer.appendChild(dot);
-  
-    // Ajout d'un event listener pour chaque point
-    dot.addEventListener('click', function () {
-      currentIndex = i;
-      updateSlide(currentIndex);
-    });
-  }
-  
-  // Appeler la fonction pour initialiser la première diapositive
-  updateSlide(currentIndex);
-  
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+// Sélection des éléments du DOM
+let slidePosition = 0;
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const bannerImg = document.querySelector("#banner .banner-img");
+const bannerTxt = document.querySelector("#banner p");
+const dots = document.querySelector(".dots");
+const pathImg = "assets/images/slideshow/"
+
+// Ecouter les flèches au clic
+
+arrowLeft.addEventListener("click", function () {
+	console.log("clic gauche");
+	slidePosition = slidePosition - 1;
+	if (slidePosition < 0) {
+		slidePosition = slides.length - 1;
+	}
+	console.log("Position",slidePosition);
+	modifySlide(slidePosition);
+	bulletPoints();
+})
+
+arrowRight.addEventListener("click", () => {
+	console.log("clic droit");
+	slidePosition = slidePosition + 1;
+	if (slidePosition > (slides.length - 1)) {
+		slidePosition = 0;
+	}
+	console.log("Position",slidePosition);
+	modifySlide(slidePosition);
+	bulletPoints();
+})
+
+
+// Création d'une fonction appelée au clic pour modifier le slide
+
+function modifySlide(id) {
+	bannerTxt.innerHTML = slides[id].tagLine;
+	bannerImg.src = pathImg + slides[id].image; // concaténer le chemin et l'image
+}
+
+function bulletPoints() {
+	i=0;
+	dots.innerHTML="";
+	for (const point of slides) {
+		console.log(i);
+		if (i === slidePosition) {
+			dots.innerHTML += "<div class='dot dot_selected'></div>";
+		} else {
+			dots.innerHTML += "<div class='dot'></div>";
+		}
+		i++;
+	}
+}
+
+// Afficher la slide initiale
+bulletPoints();
+modifySlide(slidePosition);
+
+
+
